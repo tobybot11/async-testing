3351239b1f6b2e009f406630b73d4d34a88779b9
Tobias Ford <toby.ford@pobox.com>
2018-01-29 13:49:19 -0500

    this worked .. adding another thread ..after a bunch of compiler wrestling

--- src/main.rs
+++ src/main.rs
@@ -2,21 +2,24 @@ extern crate futures;
 
 use std::io::{self, BufRead, Error};
 use std::thread;
+use std::time::Duration;
 
 use futures::{Future, Sink, Stream, Poll};
 use futures::stream::BoxStream;
 use futures::sync::mpsc::channel;
 use futures::future::Map;
 
-fn add_echo<F>(future: F) -> Map<F, fn(String) -> String>
-    where F: Future<Item=String>
-{
-    fn add(s: String) -> String { format!("echo: {:?}",s) }
-    future.map(add)
-}
+// fn add_echo<F>(future: F) -> Map<F, fn(String) -> String>
+//     where F: Future<Item=String>
+// {
+//     fn add(s: String) -> String { format!("echo: {:?}",s) }
+//     future.map(add)
+// }
 
 fn stdin() -> BoxStream<String, io::Error> {
-    let (mut tx, rx) = channel(1);
+    let (mut tx, rx) = channel(2);
+    let tx1 = tx.clone();
+
     thread::spawn(move || {
         let input = io::stdin();
         for line in input.lock().lines() {
@@ -26,6 +29,12 @@ fn stdin() -> BoxStream<String, io::Error> {
                 Err(_) => break,
             }
         }
+    });
+
+    thread::spawn(move || {
+        thread::sleep(Duration::from_secs(1));
+        let f = format!("countdown!");
+        tx1.send(Ok(f)).wait();
     });
 
     return rx.then(|e| e.unwrap()).boxed();
