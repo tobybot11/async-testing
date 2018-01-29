58048277d4031ddec348e94dd76f256487945d86
Tobias Ford <toby.ford@pobox.com>
2018-01-29 13:36:25 -0500

    this ran like this

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,26 @@
 extern crate futures;
 
-use std::io::{self, BufRead};
+use std::io::{self, BufRead, Error};
 use std::thread;
 
-use futures::{Future, Sink, Stream};
+use futures::{Future, Sink, Stream, Poll};
 use futures::stream::BoxStream;
 use futures::sync::mpsc::channel;
+use futures::future::Map;
+
+fn add_echo<F>(future: F) -> Map<F, fn(String) -> String>
+    where F: Future<Item=String>
+{
+    fn add(s: String) -> String { format!("echo: {:?}",s) }
+    future.map(add)
+}
 
 fn stdin() -> BoxStream<String, io::Error> {
     let (mut tx, rx) = channel(1);
     thread::spawn(move || {
         let input = io::stdin();
         for line in input.lock().lines() {
+//            let line = add_echo(line);
             match tx.send(line).wait() {
                 Ok(s) => tx = s,
                 Err(_) => break,
